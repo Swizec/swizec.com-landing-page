a147e9555e2eb66714a2a08c274e963ae42da441
Swizec Teller <swizec@swizec.com>
2018-05-14 21:15:49 -0700

    Get typography sorted

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,47 +1,52 @@
-const { createFilePath } = require(`gatsby-source-filesystem`);
-const path = require(`path`);
+const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require(`path`)
 
 exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
-    const { createNodeField } = boundActionCreators;
+  const { createNodeField } = boundActionCreators
 
-    if (node.internal.type === `MarkdownRemark`) {
-        const slug = createFilePath({ node, getNode, basePath: `pages` });
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: `pages`,
+    })
 
-        createNodeField({
-            node,
-            name: `slug`,
-            value: slug
-        });
-    }
-};
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
 
 exports.createPages = ({ graphql, boundActionCreators }) => {
-    const { createPage } = boundActionCreators;
-    return new Promise((resolve, reject) => {
-        graphql(`
-            {
-                allMarkdownRemark {
-                    edges {
-                        node {
-                            fields {
-                                slug
-                            }
-                        }
-                    }
-                }
+  const { createPage } = boundActionCreators
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
             }
-        `).then(result => {
-            result.data.allMarkdownRemark.edges.map(({ node }) => {
-                createPage({
-                    path: node.fields.slug,
-                    component: path.resolve(`./src/templates/workshop-page.js`),
-                    context: {
-                        // Data passed to context is available in page queries as GraphQL variables.
-                        slug: node.fields.slug
-                    }
-                });
-            });
-            resolve();
-        });
-    });
-};
+          }
+        }
+      }
+    `).then(result => {
+      result.data &&
+        result.data.allMarkdownRemark.edges.map(({ node }) => {
+          createPage({
+            path: node.fields.slug,
+            component: path.resolve(`./src/templates/workshop-page.js`),
+            context: {
+              // Data passed to context is available in page queries as GraphQL variables.
+              slug: node.fields.slug,
+            },
+          })
+        })
+      resolve()
+    })
+  })
+}
