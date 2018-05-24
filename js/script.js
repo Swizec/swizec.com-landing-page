f16174f1712f7e42d28f538c6c900c33abfbf4fb
Swizec Teller <swizec@swizec.com>
2018-05-25 00:41:00 +0200

    Fix navigation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-
 (function ($) {
     $(document).ready(function () {
-        $("header").sticky({topSpacing: -20,
-                            className: "stickied"});
+        $("header").sticky({
+            topSpacing: -20,
+            className: "stickied"
+        }).on('sticky-start', function () {
+            $("h3").addClass("stickied");
+        }).on('sticky-end', function () {
+            $("h3").removeClass("stickied");
+        });
     });
 })(jQuery);
